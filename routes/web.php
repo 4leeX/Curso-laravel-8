cf2b756cf2ea01d9071a22ea8dc9d08dc4fd7d99
Alex <alexsandro.sousa12366@gmail.com>
2021-01-24 12:41:30 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,20 @@ use App\Http\Controllers\{
 use Illuminate\Support\Facades\Route;
 
 
+Route::middleware(['auth'])->group(function(){
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::any('/post/search', [PostController::class, 'search'])->name('posts.search');
+});
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::any('/post/search', [PostController::class, 'search'])->name('posts.search');
 
 
-Route::any('/dashboard', function(){
+Route::get('/dashboard', function(){
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
